8740e42c9d7b600dc5c974f297aecb4d8e73aa8a
kelvin9314 <kelvin9314@gmail.com>
2022-02-18 16:51:03 +0800

    feat: render station Marker and Cluster

--- src/components/OpenStreetMap.tsx
+++ src/components/OpenStreetMap.tsx
@@ -1,9 +1,13 @@
+import type { LatLng } from '@/utils/constant'
+
 import React from 'react'
 import * as R from 'ramda'
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
-
-import { areaConfig, zoomLevelConfig, CENTER_OF_TAIWAN } from '@/utils/constant'
 import useStations from '@/hooks/useStations'
+import { areaConfig, zoomLevelConfig, CENTER_OF_TAIWAN } from '@/utils/constant'
+import { searchStationByName, getStationMarkerIcon, filterIncorrectStation } from '@/utils/station-helpers'
+
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMapEvents } from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-markercluster'
 
 const TAICHUNG_LAT_LNG: [number, number] = [24.154712, 120.664265]
 
@@ -16,50 +20,79 @@ const OSM_CONFIG = {
 const OpenStreetMap = () => {
   const { data: rawStations, isError, isLoading } = useStations()
 
+  const [selectedBikeType, setSelectedBikeType] = React.useState<'yb1' | 'yb2'>('yb1')
+
+  const { stationAll, stationByBikeType } = React.useMemo(() => {
+    const yb1 = (rawStations.yb1 ? filterIncorrectStation(R.clone(rawStations.yb1)) : []) as []
+    const yb2 = (rawStations.yb2 ? filterIncorrectStation(R.clone(rawStations.yb2)) : []) as []
+
+    // console.log(yb1)
+    const processingData = s => {
+      const area = R.values(areaConfig).find(area => area.areaCode === s.area_code)
+      return {
+        ...s,
+        markerIcon: getStationMarkerIcon(s),
+        areaName: area.name,
+      }
+    }
+    const stationAll = R.map(processingData, R.concat(yb1, yb2))
+    const stationByBikeType = R.map(processingData, selectedBikeType === 'yb1' ? yb1 : yb2)
+
+    return { stationAll, stationByBikeType }
+  }, [rawStations, selectedBikeType])
+
+  // React.useEffect(() => {
+  //   console.log(stationAll)
+  // }, [stationAll])
+
   React.useEffect(() => {
-    console.log(rawStations)
-  }, [rawStations])
-
-  const selectedBikeType = React.useMemo(() => {
-    const params = new URLSearchParams(location.search)
-    const val = params.get('bike_type')
-    if (['1', '2'].includes(val)) return val
-
-    // NOTE : 預設是 1.0
-    return '1'
-  }, [location.search])
-
-  // const { stationAll, stationByBikeType } = React.useMemo(() => {
-  //   const yb1 = rawStations.yb1 ? R.clone(rawStations.yb1) : []
-  //   const yb2 = rawStations.yb2 ? R.clone(rawStations.yb2) : []
-
-  //   const processingData = s => {
-  //     const area = R.values(areaConfig).find(area => area.areaCode === s.area_code)
-  //     return {
-  //       ...s,
-  //       markerIcon: getStationMarkerIcon(s),
-  //       areaName: area.name,
-  //     }
-  //   }
-  //   const stationAll = R.map(processingData, R.concat(yb1, yb2))
-  //   const stationByBikeType = R.map(processingData, selectedBikeType === '1' ? yb1 : yb2)
-
-  //   return { stationAll, stationByBikeType }
-  // }, [rawStations, selectedBikeType])
+    console.log(stationByBikeType)
+  }, [stationByBikeType])
 
   return (
     <div className="station-map-container">
-      <MapContainer className="osm" center={CENTER_OF_TAIWAN} zoom={8} maxZoom={8}>
+      <MapContainer className="osm" center={CENTER_OF_TAIWAN} zoom={8} minZoom={8}>
         <TileLayer url={OSM_CONFIG.url} attribution={OSM_CONFIG.attribution} />
-        {/* <Marker position={areaConfig.taichung.position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable. Easily customizable.Easily customizable.Easily
-            customizable.Easily customizable.Easily customizable.
-          </Popup>
-        </Marker> */}
+        <MarkerClusterGroup>
+          {stationAll?.map(station => {
+            const position: LatLng = [+station.lat, +station.lng]
+
+            return (
+              <Marker key={station.station_no} position={position}>
+                <Popup>{station.name_tw}</Popup>
+              </Marker>
+            )
+            // return <StationMarker key={station.station_no} position={[+station.lat, +station.lng]} station={station} />
+          })}
+        </MarkerClusterGroup>
       </MapContainer>
     </div>
   )
 }
 
 export default OpenStreetMap
+
+type StationMarkerProps = {
+  position: LatLng
+  station: {}
+}
+
+// const StationMarker = ({ station, position }) => {
+//   const map = useMapEvents({
+//     click() {
+//       console.log('marker clicked')
+//       map.locate()
+//     },
+//     locationfound(e) {
+//       console.log(e)
+//       // setPosition(e.latlng)
+//       // map.flyTo(e.latlng, map.getZoom())
+//     },
+//   })
+
+//   return (
+//     <Marker position={position}>
+//       <Popup>{station.name_tw}</Popup>
+//     </Marker>
+//   )
+// }
